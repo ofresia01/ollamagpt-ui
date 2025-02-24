abb820a5a57cb014aef2e3b47aa243d3860383d5
Owen Fresia <ofresia01@gmail.com>
2025-02-24 01:28:48 -0600

    style: prettier formatting

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [sveltekit(), tailwindcss()],
   server: {
     fs: {
-      allow: [searchForWorkspaceRoot(process.cwd())]
-    }
-  }
+      allow: [searchForWorkspaceRoot(process.cwd())],
+    },
+  },
 });
