bf3dd425412245f6ad7ce6f77c0c7491895080b1
Owen Fresia <ofresia01@gmail.com>
2025-02-24 01:27:50 -0600

    chore(svelte-config): made ts

--- svelte.config.ts
+++ svelte.config.ts
@@ -2,7 +2,7 @@ import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+const config: import('@sveltejs/kit').Config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
